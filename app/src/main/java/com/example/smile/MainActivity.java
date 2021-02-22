1eab83485b5f25281bfce7b83729698046d766a7
anpalmak1 <73543260+anpalmak1@users.noreply.github.com>
2021-02-22 12:33:56 +0400

    first commit

---
+++ app/src/main/java/com/example/smile/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.smile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawView(this));
+    }
+}
